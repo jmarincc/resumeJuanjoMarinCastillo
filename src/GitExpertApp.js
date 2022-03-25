f28165add5e3bfb93d3cabeebe06493151aedf3b
Juanjogit config --global user.name Juanjogit config --global user.name Juanjo <juanjo.marincastillo@gmail.com>
2022-03-25 07:14:08 +0100

    curri3

--- src/GitExpertApp.js
+++ src/GitExpertApp.js
@@ -11,7 +11,7 @@ export const GitExpertApp = () => {
   //}
   return(
     <>
-        <h2>CURRICULUM JUANJO MARÍN CASTILLOooo</h2>
+        <h2>CURRICULUM JUANJO MARÍN CASTILLOoeeeoo</h2>
         <AddCategory setCategories={setCategories}/>
         <hr/>
        
